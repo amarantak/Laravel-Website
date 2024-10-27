89e0521a62f818754c453604bb5e89c4b3ae89f4
amy <amarantakruger@gmail.com>
2024-10-27 20:23:26 +0100

    Access Control

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -8,6 +8,16 @@ use App\Models\Post;
 class PostsController extends Controller
 {
     /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['index', 'show']]);
+    }
+
+    /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
@@ -75,6 +85,10 @@ class PostsController extends Controller
     public function edit($id)
     {
         $post = Post::find($id);
+        //Check for correct user
+        if (auth()->user()->id !== $post->user_id) {
+            return redirect('/posts')->with('error', 'Unauthorized Page');
+        }
 
         return view('posts.edit')->with('post', $post);
     }
@@ -112,6 +126,12 @@ class PostsController extends Controller
     public function destroy($id)
     {
         $post = Post::find($id);
+
+        //Check for correct user
+        if (auth()->user()->id !== $post->user_id) {
+            return redirect('/posts')->with('error', 'Unauthorized Page');
+        }
+
         $post->delete();
         return redirect('/posts')->with('success', 'Post Removed');
     }
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,25 +2,29 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>{{ $post->title }}</h1>  
-    <a href="{{ url('/posts') }}" class="btn btn-default">Go Back</a>   
+    <h1>{{ $post->title }}</h1>
+    <a href="{{ url('/posts') }}" class="btn btn-default">Go Back</a>
     <div>
         {!! $post->body !!} <!-- Use with caution, ensure content is sanitized -->
     </div>
     <hr>
     <small>Written on {{ $post->created_at->format('M d, Y') }} by {{$post->user->name}}</small>
     <hr>
-    <div class="btn-group" role="group" aria-label="Post Actions" style="left: right;">
-        <div>
-            <a href="/posts/{{$post->id}}/edit" class="btn btn-primary">Edit</a>
-        </div>
-        &nbsp;&nbsp;
-        <div class="pl-5">
-            {!! Form::open(['action' => ['App\Http\Controllers\PostsController@destroy', $post->id], 'method' => 'POST', 'class' => '']) !!}
-            {{ Form::hidden('_method', 'DELETE') }}
-            {{ Form::submit('Delete', ['class' => 'btn btn-danger']) }}
-        {!! Form::close() !!}
-        </div>
-       
+    {{-- Show buttons only if auth user --}}
+    @if(!Auth::guest())
+        @if(Auth::user()->id == $post->user_id)
+        <div class="btn-group" role="group" aria-label="Post Actions" style="left: right;">
+            <div>
+                <a href="/posts/{{$post->id}}/edit" class="btn btn-primary">Edit</a>
+            </div>
+            &nbsp;&nbsp;
+            <div class="pl-5">
+                {!! Form::open(['action' => ['App\Http\Controllers\PostsController@destroy', $post->id], 'method' => 'POST', 'class' => '']) !!}
+                {{ Form::hidden('_method', 'DELETE') }}
+                {{ Form::submit('Delete', ['class' => 'btn btn-danger']) }}
+            {!! Form::close() !!}
+            </div>
+        @endif
+    @endif
     </div>
 @endsection
